8f229ca37f55ced02b097ce35d05780a87f5c6a9
jsdevuz <jsdevuz.me@gmail.com>
2025-12-28 15:24:48 +0500

    add cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://gapbor.jsdev.uz",
+  "https://gapbor-server.onrender.com",
   "192.168.100.253:3000",
   "http://192.168.100.253:3000"
   
